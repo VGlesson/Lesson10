eaa800164c8708740fd56c064b8b730890515209
Владимир Гаврилов <saxar.1kg@mail.ru>
2022-10-29 19:36:35 +0300

    Добавьте файлы проекта.

---
+++ Task1/Class Angle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Class_Angle
+    {
+        public int gradus;
+        public int minutes;
+        public int second;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value > -360 || value > 360)
+                {
+                    Console.WriteLine("Значение не корректное");
+                }
+                else
+                {
+                    gradus = value;
+                }
+            }
+        }
+         public int Minutes
+        {
+            get
+            {
+                return minutes;
+            }
+            set
+            {
+                if (value < 0 || value > 60)
+                {
+                    Console.WriteLine("Значение не корректное");
+                }
+                else
+                {
+                    minutes = value;
+                }
+            }
+         }
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                if (value < 0 || value > 60)
+                {
+                    Console.WriteLine("Значение не корректное");
+                }
+                else
+                {
+                    second = value;
+                }
+            }
+        }
+            public Class_Angle(int gradus, int minutes, int second)
+            {
+            this.Gradus = gradus;
+            this.Minutes = minutes;
+            this.Second = second;
+            }
+
+        public Class_Angle()
+        {
+        }
+
+        public double ConvertToRadians(double angle)
+            {
+            return (Math.PI / 180) * angle;
+            }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class_Angle angle = new Class_Angle();
+            Console.WriteLine("Задайте угол с помощью целочисленных значений");
+            Console.WriteLine("Введите градус");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите минуты");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите cекунды");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(angle);
+            Console.ReadKey();
+        }
+         double ConvertToRadians(double angle)
+        {
+            return (Math.PI / 180) * angle;
+        }
+    }
+}
